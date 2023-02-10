589ac364bb23c84dee3bf5cf842f8c054719eba1
DammyD <ibukunoluwadawodu@gmail.com>
2023-02-10 11:25:54 +0100

    :zap: js-basics

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * create variables for your first name, last name, Stutern track, skills, 
+ * favourite color, year of your country’s independence. 
+ * Use these variables to form a sentence and log the sentence in the console.
+ * */
+
+let firstName = 'Blessing';
+let lastName = 'Dawodu';
+let stuternTrack = 'Frontend Engineer';
+let skills = 'web development';
+let favoriteColor = 'Black';
+let countryIndependence = 1960;
+
+console.log(firstName);
+console.log(lastName);
+console.log(stuternTrack);
+console.log(skills);
+console.log(favoriteColor);
+console.log(countryIndependence);
+
+console.log(`My name is ${firstName} ${lastName}. I am a ${stuternTrack} learning at Stutern. I have some basic ${skills} skills. My favorite color is ${favoriteColor}. The country I live in got her independence ${countryIndependence}.
+`);
+
